6ba18d0e08dd44dbb2116b172871e47448b82997
MaluPessoa <malumansopessoa@gmail.com>
2026-03-24 13:51:29 -0300

    Projeto correto

--- cards/src/App.jsx
+++ cards/src/App.jsx
@@ -14,56 +14,12 @@ import lifeisstrangeImg from './assets/imgs/lifeisstrange.jpeg';
 
 function App() {
   const Fundamentos = [
-HEAD,
-    {
-      id: 1,
-      titulo: 'O que é React',
-      conteudo: 'React é uma biblioteca JS para criar interfaces.',
-      exemplo: 'Exemplo de uso',
-      codigo: "COD001",
-      capa: stardewImg
-    },
-    {
-      id: 2,
-      titulo: 'The Sims 4',
-      conteudo: 'Simulação de Vida',
-      exemplo: 'Gratuito',
-      codigo: 'COD002',
-      capa: simsImg
-    },
-    {
-      id: 3,
-      titulo: 'Animal Crossing',
-      conteudo: 'Simulação / Casual',
-      exemplo: '199.90',
-      codigo: 'COD003',
-      capa: animalcrossingImg
-    },
-    {
-      id: 4,
-      titulo: 'Overcooked 2',
-      conteudo: 'Cooperativo / Party Game',
-      exemplo: '49.90',
-      codigo: 'COD004',
-      capa: overcookedImg
-    },
-    {
-      id: 5,
-      titulo: 'Unpacking',
-      conteudo: 'Puzzle / Relaxante',
-      exemplo: '59.90',
-      codigo: 'COD005',
-      capa: unpackingImg
-    },
-    {
-      id: 6,
-      titulo: 'Hogwarts Legacy',
-      conteudo: 'RPG / Mundo Aberto',
-      exemplo: '149.90',
-      codigo: 'COD006',
-      capa: hogwartsImg
-    },
-
+    { id: 1, titulo: 'O que é React', conteudo: 'React é uma biblioteca JS para criar interfaces.', exemplo: 'Exemplo de uso', codigo: "", capa: stardewImg },
+    { id: 2, titulo: 'The Sims 4', conteudo: 'Simulação de Vida', exemplo: 'Gratuito', codigo: 'COD002', capa: simsImg },
+    { id: 3, titulo: 'Animal Crossing', conteudo: 'Simulação / Casual', exemplo: '199.90', codigo: 'COD003', capa: animalcrossingImg },
+    { id: 4, titulo: 'Overcooked 2', conteudo: 'Cooperativo / Party Game', exemplo: '49.90', codigo: 'COD004', capa: overcookedImg },
+    { id: 5, titulo: 'Unpacking', conteudo: 'Puzzle / Relaxante', exemplo: '59.90', codigo: 'COD005', capa: unpackingImg },
+    { id: 6, titulo: 'Hogwarts Legacy', conteudo: 'RPG / Mundo Aberto', exemplo: '149.90', codigo: 'COD006', capa: hogwartsImg },
   ];
 
   const JSX = [
@@ -82,16 +38,20 @@ HEAD,
   
   ];
 
-  const Hooks = [
-    { id: 27, titulo: 'useState', conteudo: 'Hook para gerenciar estado.', exemplo: 'const [count, setCount] = useState(0)', codigo: 'HOOK001', capa: animalcrossingImg },
-    { id: 28, titulo: 'useEffect', conteudo: 'Hook para efeitos colaterais.', exemplo: 'useEffect(() => {...}, [])', codigo: 'HOOK002', capa: overcookedImg },
-    { id: 29, titulo: 'useEffect', conteudo: 'Hook para efeitos colaterais.', exemplo: 'useEffect(() => {...}, [])', codigo: 'HOOK002', capa: overcookedImg },
-
+  const Props = [
+    { id: 16, titulo: 'Componentes Funcionais', conteudo: 'Funções que retornam JSX.', exemplo: 'function App() { return <h1>Oi</h1> }', codigo: 'COMP001', capa: simsImg },
+    { id: 17, titulo: 'Props', conteudo: 'Permite passar dados para componentes.', exemplo: '<Card titulo="React"/>', codigo: 'COMP002', capa: stardewImg },
+    { id: 18, titulo: 'Componentes Funcionais', conteudo: 'Funções que retornam JSX.', exemplo: 'function App() { return <h1>Oi</h1> }', codigo: 'COMP001', capa: simsImg },
+    { id: 19, titulo: 'Componentes Funcionais', conteudo: 'Funções que retornam JSX.', exemplo: 'function App() { return <h1>Oi</h1> }', codigo: 'COMP001', capa: simsImg },
+  
   ];
 
-  const Router = [
-    { id: 1, titulo: 'React Router', conteudo: 'Biblioteca para navegação.', exemplo: '<Route path="/home" />', codigo: 'ROUT001', capa: unpackingImg },
-    { id: 2, titulo: 'Link', conteudo: 'Componente para navegação.', exemplo: '<Link to="/about">Sobre</Link>', codigo: 'ROUT002', capa: hogwartsImg },
+  const State = [
+    { id: 20, titulo: 'Componentes Funcionais', conteudo: 'Funções que retornam JSX.', exemplo: 'function App() { return <h1>Oi</h1> }', codigo: 'COMP001', capa: simsImg },
+    { id: 21, titulo: 'Props', conteudo: 'Permite passar dados para componentes.', exemplo: '<Card titulo="React"/>', codigo: 'COMP002', capa: stardewImg },
+    { id: 22, titulo: 'Componentes Funcionais', conteudo: 'Funções que retornam JSX.', exemplo: 'function App() { return <h1>Oi</h1> }', codigo: 'COMP001', capa: simsImg },
+    { id: 23, titulo: 'Componentes Funcionais', conteudo: 'Funções que retornam JSX.', exemplo: 'function App() { return <h1>Oi</h1> }', codigo: 'COMP001', capa: simsImg },
+  
   ];
 
   const Renderizacao = [
@@ -100,6 +60,21 @@ HEAD,
     { id: 26, titulo: 'Context API', conteudo: 'Gerencia estado global.', exemplo: '<Context.Provider>', codigo: 'STATE001', capa: simsImg },
   ];
 
+  const Hooks = [
+    { id: 27, titulo: 'useState', conteudo: 'Hook para gerenciar estado.', exemplo: 'const [count, setCount] = useState(0)', codigo: 'HOOK001', capa: animalcrossingImg },
+    { id: 28, titulo: 'useEffect', conteudo: 'Hook para efeitos colaterais.', exemplo: 'useEffect(() => {...}, [])', codigo: 'HOOK002', capa: overcookedImg },
+    { id: 29, titulo: 'useEffect', conteudo: 'Hook para efeitos colaterais.', exemplo: 'useEffect(() => {...}, [])', codigo: 'HOOK002', capa: overcookedImg },
+
+  ];
+
+  const Conceitosextras = [
+    { id: 30, titulo: 'React Router', conteudo: 'Biblioteca para navegação.', exemplo: '<Route path="/home" />', codigo: 'ROUT001', capa: unpackingImg },
+  ];
+
+  const Router = [
+    { id: 1, titulo: 'React Router', conteudo: 'Biblioteca para navegação.', exemplo: '<Route path="/home" />', codigo: 'ROUT001', capa: unpackingImg },
+    { id: 2, titulo: 'Link', conteudo: 'Componente para navegação.', exemplo: '<Link to="/about">Sobre</Link>', codigo: 'ROUT002', capa: hogwartsImg },
+  ];
 
   return (
     <div className="app">
@@ -135,12 +110,38 @@ HEAD,
         </section>
 
         {/* COMPONENTS */}
-        <div id="components"><h1>COMPONENTS</h1></div>
+        <div id="components"><h1>COMPONENTES</h1></div>
         <section className="Components-grid">
           {Components.map(item => (
             <CardJogo key={item.id} {...item} />
           ))}
         </section>
+
+                {/* PROPS */}
+        <div id="props"><h1>PROPS</h1></div>
+        <section className="Props-grid">
+          {Props.map(item => (
+            <CardJogo key={item.id} {...item} />
+          ))}
+        </section>
+
+                {/* STATE */}
+        <div id="state"><h1>STATE</h1></div>
+        <section className="State-grid">
+          {State.map(item => (
+            <CardJogo key={item.id} {...item} />
+          ))}
+        </section>
+
+
+        {/* RENDERIZAÇÃo */}
+        <div id="renderizacao"><h1>RENDERIZAÇÃO</h1></div>
+        <section className="renderizacao-grid">
+          {Renderizacao.map(item => (
+            <CardJogo key={item.id} {...item} />
+          ))}
+        </section>
+
 
         {/* HOOKS */}
         <div id="hooks"><h1>HOOKS</h1></div>
@@ -157,14 +158,15 @@ HEAD,
             <CardJogo key={item.id} {...item} />
           ))}
         </section>
-
-        {/* STATE MANAGEMENT */}
-        <div id="renderizacao"><h1>RENDERIZAÇÂO</h1></div>
-        <section className="renderizacao-grid">
-          {Renderizacao.map(item => (
+        
+        {/* Conceitos */}
+        <div id="Conceitosextras"><h1>CONCEITOS EXTRAS</h1></div>
+        <section className="Conceitoextras-grid">
+          {Conceitosextras.map(item => (
             <CardJogo key={item.id} {...item} />
           ))}
         </section>
+
       </main>
     </div>
   );
